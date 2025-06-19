be85368d6125d5a8e61dbf5fe2cbbe188ac6efab
mayur7267 <mayurlate.7267@gmail.com>
2025-06-19 19:26:35 +0530

    Initial Commit

---
+++ PortfolioApp.swift/PortfolioApp_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp_swiftApp.swift
+//  PortfolioApp.swift
+//
+//  Created by Mayur on 19/06/25.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PortfolioApp.swiftTests/PortfolioApp_swiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp_swiftTests.swift
+//  PortfolioApp.swiftTests
+//
+//  Created by Mayur on 19/06/25.
+//
+
+import Testing
+@testable import PortfolioApp_swift
+
+struct PortfolioApp_swiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
